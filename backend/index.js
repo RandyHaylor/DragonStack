e6cb3fa258462155db0f6f7bb16580144f57a755
Randy Haylor <rhaylor@yahoo.com>
2019-05-08 16:24:48 -0400

    Completed through 3.26

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,39 @@
+const express = require('express');
+const GenerationEngine = require('./engine');
+
+const app = express();
+const engine = new GenerationEngine();
+const port = 3001;
+
+engine.start();
+
+app.get('/dragon/new', (req, res) => {
+    res.json({ dragon: engine.generation.newDragon() });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+
+/* CODE FROM EARLIER IN COURSE, second time it's removed
+const Generation = require('./generation');
+
+const generation = new Generation();
+
+console.log('generation', generation);
+
+const gooby = generation.newDragon();
+
+console.log('gooby',gooby);
+
+setTimeout(() => {
+    const mimar = generation.newDragon();
+    console.log('mimar',mimar);
+}, 15000);
+*/
+
+
+/*  CODE FROM EARLIER IN COURSE
 const Dragon = require('./dragon.js');
 
 const fooey = new Dragon({
@@ -6,7 +42,12 @@ const fooey = new Dragon({
 });
 const baloo = new Dragon({
     birthdate: new Date(),
-    nickname: 'baloo'
+    nickname: 'baloo',
+    traits: [
+        {
+            traitType: 'backgroundColor', traitValue: 'green'
+        }
+    ]
 });
 
 const mimar = new Dragon();
@@ -20,4 +61,4 @@ console.log('fooey', fooey);
 console.log('baloo', baloo);
 
 
-console.log('mimar', mimar);
+console.log('mimar', mimar); */
